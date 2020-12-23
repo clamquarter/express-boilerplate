fabac0c2b0c45e09dc369f956b9d7336c7ec6c6c
India Walker <indiadoria@gmail.com>
2020-12-23 16:11:55 -0500

    fixed boilerplate

--- src/app.js
+++ src/app.js
@@ -3,10 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const { NODE_ENV, API_TOKEN } = require('./config')
-const bookmarksRouter = require('./bookmarks-router')
-const logger = require('./logger')
-
+const { NODE_ENV } = require('./config')
 
 const app = express()
 
@@ -14,39 +11,24 @@ const morganOption = (NODE_ENV === 'production')
 ? 'tiny'
 : 'common';
 
-function validateBearerToken(req, res, next) {
-  const authToken = req.get('Authorization')
-  logger.error(`Unauthorized request to path: ${req.path}`)
-
-  if (!authToken || authToken.split(' ')[1] !== API_TOKEN) {
-    return res.status(401).json({ error: 'Unauthorized request' })
-  }
-
-  next()
-}
-
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
-app.use(validateBearerToken)
-app.use(bookmarksRouter)
-
-
-
-app.get('/', (req, res) => {
-    
+app.use(function errorHandler(error, req, res, next) {
+       let response
+       if (NODE_ENV === 'production') {
+         response = { error: { message: 'server error' } }
+       } else {
+         console.error(error)
+         response = { message: error.message, error }
+       }
+       res.status(500).json(response)
      })
 
-     app.use(function errorHandler(error, req, res, next) {
-      let response
-      if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
-      } else {
-        console.error(error)
-        response = { message: error.message, error }
-      }
-      res.status(500).json(response)
-    })
+app.get('/', (req, res) => {
+    res.send('Hello, boilerplate!')
+     })
+
 
 
 module.exports = app
